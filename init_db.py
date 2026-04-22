b4ddfa414809e533491be45109ad2fa56d3737dd
Svitlana <G00473000@atu.ie>
2026-04-22 18:29:10 +0000

    update db: create first tables for our database

--- init_db.py
+++ init_db.py
@@ -1,6 +1,21 @@
 import sqlite3
 
+# creating a db file
 connection = sqlite3.connect('app.db')
+
+# creating a cursor to execute SQL commands
+db_cursor = connection.cursor()
+
+# adding info to tables
+db_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name TEXT NOT NULL,
+        course_group TEXT NOT NULL
+    )
+''')
+
+# updating the db with data
+connection.commit()
 connection.close()
-    
 
