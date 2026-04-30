7471309e6dc032019857e578550758138c6a44b5
Svitlana <G00473000@atu.ie>
2026-04-30 14:25:46 +0000

    create db_config with connection data, totally changed student DAO, using mysql

--- studentDAO.py
+++ studentDAO.py
@@ -1,41 +1,22 @@
-import sqlite3
-
-# Database file name
-DB_FILE = 'app.db'
+from db_config import get_db_connection
 
 def get_all_students():
-    # 1. Establish connection to the database
-    conn = sqlite3.connect(DB_FILE)
-    
-    # 2. Set row_factory to Row to access columns by name
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    
-    # 3. Execute the selection query
-    cursor.execute("SELECT * FROM students")
-    rows = cursor.fetchall()
-    
-    # 4. Convert Row objects into a standard list of dictionaries
-    students = []
-    for row in rows:
-        students.append(dict(row))
-    
-    # 5. Close connection and return the data
-    conn.close()
-    return students
+    db = get_db_connection()
+    cursor = db.cursor()
+    sql = "SELECT * FROM student"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return result
 
-def add_student(name, group):
-    # 1. Open connection
-    conn = sqlite3.connect(DB_FILE)
-    cursor = conn.cursor()
-    
-    # 2. Prepare and execute the insert command using parameterized queries
-    sql = "INSERT INTO students (full_name, course_group) VALUES (?, ?)"
-    values = (name, group)
+def get_student_by_id(student_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    sql = "SELECT * FROM student WHERE id = %s"
+    values = (student_id,)
     cursor.execute(sql, values)
-    
-    # 3. Commit changes to the database
-    conn.commit()
-    
-    # 4. Close connection
-    conn.close()
+    result = cursor.fetchone()
+    cursor.close()
+    db.close()
+    return result
