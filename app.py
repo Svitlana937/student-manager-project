d3e22401adec55a44cb0587a942c7dd05e4a405f
Svitlana <G00473000@atu.ie>
2026-05-08 14:31:27 +0000

    Initialize student manager routes with MySQL DAO

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import studentDAO
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
+    students = studentDAO.get_all_students()
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
