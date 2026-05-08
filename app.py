a5d0ff3a1851304964256865a8a60ae85630ccba
Svitlana <G00473000@atu.ie>
2026-05-08 14:39:23 +0000

    Add get_student_id route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import studentDAO
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -7,11 +8,19 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     students = studentDAO.get_all_students()
-    return render_template("index.html")
+    return render_template("index.html", students=students)
 
 def get_student(student_id):
     student = studentDAO.get_student_by_id(student_id)
     return render_template("student.html", student=student)
+
+@app.route("/student/<int:student_id>", methods=["GET"])
+def get_student_id(student_id):
+    student = studentDAO.get_student_by_id(student_id)
+    if student:
+        return jsonify(student)
+    else:
+        return jsonify({"error": "Student not found"}), 404
 
 
 if __name__ == "__main__":
